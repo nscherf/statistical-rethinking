bb72845a98de18256f079064d4990c64e309aed7
nscherf <nscherf@googlemail.com>
2018-03-26 20:36:16 +0200

    source of examples from book

---
+++ src/ch-02.R
@@ -0,0 +1,12 @@
+library(rethinking)
+library(ggplot2)
+
+p_grid <- seq(from=0, to=1, length.out = 20) 
+prior <- rep(1, 20)
+likelihood <- dbinom(6, size=9, prob = p_grid)
+unstd.posterior <- likelihood * prior
+posterior <- unstd.posterior / sum(unstd.posterior)
+p_data <- data.frame(p=p_grid, posterior=posterior)
+
+ggplot(data=p_data, aes(x=p, y=posterior)) + geom_line()
+
