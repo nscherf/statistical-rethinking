42a77519f88413f1cc807661e3ae4ea93b779a7f
nscherf <nscherf@spinoza.fritz.box>
2021-03-26 20:16:08 +0100

    add example 4.50 and 4.51 conditional posterior distribution

--- src/linear-regression-posterior-samples.R
+++ src/linear-regression-posterior-samples.R
@@ -20,15 +20,22 @@ mN <- quap(
   ) , data = dN)
 
 # sample from the posterior
-post <- extract.samples( mN, n=20 )
+n_posterior_samples <- 20
+post <- extract.samples( mN, n=n_posterior_samples )
 
 plot( dN$weight, dN$height , 
       xlim = range(d2$weight) , ylim = range(d2$height) , 
       col = rangi2, xlab = "weight" , ylab = "height")
       mtext(concat("N = ", N))
       
-for ( i in 1:20 )
+for ( i in 1:n_posterior_samples )
   curve (post$a[i] + post$b[i]*(x-mean(dN$weight)) ,
          col = col.alpha("black", 0.3), add = TRUE)
+
+# extract the posterior distribution of  mean height (mu) for a (conditional on a) given weight (x value).
+post <- extract.samples(mN)
+xbar <- mean(dN$weight)
+mu_at_47 <- post$a + post$b * ( 47 - xbar )
+dens( mu_at_47, col=rangi2, lwd=2, xlab="mu|weight = 47")
 
       
