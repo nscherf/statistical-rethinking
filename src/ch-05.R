1214110ca3fc58cfd96ee7ea1129113b86fb9f6b
nscherf <nscherf@googlemail.com>
2021-05-25 15:33:52 +0200

    fix typo in model 5.1

--- src/ch-05.R
+++ src/ch-05.R
@@ -11,14 +11,14 @@ precis(d)
 m5.1 <- quap(
   alist(
     D ~ dnorm(mu, sigma),
-    mu <- a + beta * A,
-    a ~ dnorm(0, 0.02),
-    beta ~ dnorm(0, 0.5),
+    mu <- a + bA * A,
+    a ~ dnorm(0, 0.2),
+    bA ~ dnorm(0, 0.5),
     sigma ~ dexp(1)
   ), data = d)
 
 set.seed(10)
 prior <- extract.prior( m5.1 ) 
-mu <- link( m5.1 , post=prior, data=list( A=c(-2,) ) )
+mu <- link( m5.1 , post=prior, data=list( A=c(-2,2) ) )
 plot(NULL, xlim=c(-2,2), ylim=c(-2,2))
 for (i in 1:50 ) lines ( c(-2, 2), mu[i, ], col = col.alpha("black", 0.4))
